00e6e377f67a9133953d884dc4f2bdaf4a05f95d
sirguse <tesfov@gmail.com>
2023-05-02 15:48:24 +0200

    first push

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+const splash = document.querySelector(".Intro");
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+  setTimeout(()=>{
+    splash.classList.add('xd');
+  }, 2000);
+})
+
+
